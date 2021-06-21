d4cb96de9c9926192aaaf323d5dc5c888efdf701
Onur <48868792+shagye@users.noreply.github.com>
2021-06-21 20:31:33 +0300

    update
    update

---
+++ Huma Automation/README.md
@@ -0,0 +1,4 @@
+Huma Otel Automation
+Group 1
+Onur Akbal , Yunus Emre Ertekin
+![alt text](https://github.com/shagye/C-Sharp-Projects/blob/master/Huma%20Automation/Photos/1.png)
---
+++ Huma Automation/huma-otel/Hostel/DBSeeder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Hostel
+{
+    public class DBSeeder : DropCreateDatabaseIfModelChanges<HostelDBContext>
+    {
+        protected override void Seed(HostelDBContext context)
+        {
+
+            User user = new User()
+            {
+                Username = "Admin",
+                Password = "admin",
+                Type = "Admin",
+                status = "Active",
+            };
+            Hostel hostel = new Hostel()
+            {
+                HostelName = "Huma Otel",
+                HostelTitle = "HUMA",
+                Email = "info@huma.com.tr",
+                Phone = "0 (312) 330 00 00",
+                City = "Muğla",
+                Area = "Akyaka",
+                Road = "2",
+                House = "13",
+                HostelLogo = "humalogo.png",
+            };
+            context.Users.Add(user);
+            context.Hostels.Add(hostel);
+            context.SaveChanges();
+            base.Seed(context);
+        }
+    }
+}
