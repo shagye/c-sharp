def932eead4680cccfd0f88c80906aec3b88c7b8
Onur <48868792+shagye@users.noreply.github.com>
2021-06-21 20:27:08 +0300

    upload
    upload

---
+++ Huma Automation/README.md
@@ -0,0 +1,4 @@
+Huma Otel Automation
+Group 1
+Onur Akbal , Yunus Emre Ertekin
+![alt text](https://github.com/[username]/[reponame]/blob/[branch]/image.jpg?raw=true)
---
+++ Huma Automation/huma-otel/Hostel/Position.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Hostel
+{
+    public class Position
+    {
+        [Key]
+        public int PositionId { get; set; }
+        [Required]
+        public string PositionName { get; set; }
+        [Required]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Sayısal Alan")]
+        public int Salary { get; set; }
+        List<Employee> Employees { get; set; }
+    }
+}
